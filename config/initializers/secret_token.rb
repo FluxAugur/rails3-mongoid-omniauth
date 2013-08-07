3488e21a7ab83bb45e6ad30ab9e6b0a19367c021
Nathanial L. McConnell <nate@nmcconnell.com>
2013-08-07 05:35:23 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails3MongoidOmniauth::Application.config.secret_key_base = '876b84d8ede818ded20d24a25eb8d2a7bc5e159248e8b3d556239b300365bb9d0f2579da938d2f0c9e4a6cfe8572cd6d006f75d3ba108aaa713cf8ea620f392c'
